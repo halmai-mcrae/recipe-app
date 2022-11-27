86265407f75e99f3025fd49099c9f72d849fbbfd
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-11-27 17:15:37 +1300

    Add recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,20 @@
-import { EventEmitter } from "@angular/core";
-import {Recipe} from "./recipe.model";
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Recipe 1', 'this is a test',
-      'https://images.unsplash.com/photo-1561583953-29d04b8cf5ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
-    new Recipe('Recipe 2', 'this is a test',
-      "https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80")
+    new Recipe(
+      'Recipe 1',
+      'this is a test',
+      'https://images.unsplash.com/photo-1561583953-29d04b8cf5ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+    ),
+    new Recipe(
+      'Recipe 2',
+      'this is a test',
+      'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+    ),
   ];
 
   getRecipes() {
