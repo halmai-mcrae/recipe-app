bd7ad000aaf0ffa15998794518ef765914895deb
Halmai McRae <halmaimcrae@Halmais-MacBook-Pro.local>
2022-11-27 16:14:57 +1300

    Continued

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,24 @@
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+    private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Spring onion', 5),
+    new Ingredient('Milk', 10),
+    new Ingredient('Pumpkin', 5),
+    new Ingredient('Chives', 10),
+    new Ingredient('Basil', 5),
+    new Ingredient('Aubergine', 10),
+    new Ingredient('Chilli oil', 5),
+    new Ingredient('Truffles', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
