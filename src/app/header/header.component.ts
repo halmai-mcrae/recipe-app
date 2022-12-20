a419e730718d808b578405de69ecb6c3201c088c
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-12-20 15:32:37 +1300

    Remove event emitters and uneeded code from header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core'
-
-
+import { Component } from '@angular/core'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
   })
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-  collapsed = true;
 
 
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature)
-  }
 
 }
