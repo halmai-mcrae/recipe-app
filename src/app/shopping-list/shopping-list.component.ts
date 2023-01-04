088afeba7114e97cae7f9067ad3dd0a6bf6b9c80
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-01-04 17:11:18 +1300

    Fix error in console

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,7 +16,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    this.subscription = this.slService.ingredientsChanged
+    .subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
   }
