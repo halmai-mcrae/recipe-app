93682b51fdd9def9478a1d9714d4a69df5a23356
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-01-04 15:00:36 +1300

    Add methods to service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,30 +1,61 @@
-import { EventEmitter } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
+  public recipeChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Recipe 1',
-      'this is a test',
-      'https://images.unsplash.com/photo-1561583953-29d04b8cf5ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-      [
-        new Ingredient('Lentils', 20),
-        new Ingredient('Pastry sheet', 15)
-      ]),
+      'Curried Lentil Pies',
+      'Plant-based, Peanut-free',
+      'https://images.unsplash.com/photo-1559143113-6332004db0e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+      [new Ingredient('Lentils', 2), new Ingredient('Pastry Sheet', 3)]
+    ),
     new Recipe(
-      'Recipe 2',
-      'this is a test',
-      'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-      [
-        new Ingredient('Buns', 40),
-        new Ingredient('Burger patty', 45)
-      ]),
+      'French Onion Dip',
+      'Plant-based, Peanut-free, Gluten-free',
+      'https://images.unsplash.com/photo-1615485925873-7ecbbe90a866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+      [new Ingredient('Cashews', 1), new Ingredient('Tahini', 4)]
+    ),
   ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipeChanged.next(this.recipes.slice());
   }
 }
