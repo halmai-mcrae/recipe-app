23585a4c16b1fcb1e1bd40bca10efbafc98e6711
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-01-16 11:27:54 +1300

    Fix syntax

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -50,8 +50,8 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount),
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount),
             })
           );
         }
@@ -59,10 +59,10 @@ export class RecipeEditComponent implements OnInit {
     }
 
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription),
-      ingredients: recipeIngredients,
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients,
     });
   }
 }
