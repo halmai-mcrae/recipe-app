8fb419030e78d40e5f48273a3fbd46d88cac5d74
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-03-05 11:24:33 +1300

    Set up firestore, add ability to save and fetch data from server

---
+++ src/app/shared/data-storage-service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://recipe-app-3a2a5-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://recipe-app-3a2a5-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+  }
+}
