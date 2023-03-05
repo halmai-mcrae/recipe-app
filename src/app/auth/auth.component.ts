91865b6e6c507d189622a05855e06ff0af5210ec
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-03-05 20:41:34 +1300

    Able to send login req

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthService, AuthResponseData } from './auth.service';
 
-import { AuthService } from './auth.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -25,22 +26,26 @@ export class AuthComponent {
     const password = form.value.password;
 
     this.isLoading = true;
-    if (this.isLoginMode) {
-      // ...
-    } else {
-      this.authService.signup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (errorMessage) => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
 
-      form.reset();
+    let authObservable: Observable<AuthResponseData>;
+
+    if (this.isLoginMode) {
+      authObservable = this.authService.login(email, password);
+    } else {
+      authObservable = this.authService.signup(email, password);
     }
+
+    authObservable.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+        form.reset();
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
   }
 }
