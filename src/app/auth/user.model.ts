ecf58f961a7efe476a9303065afa10453c69317f
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-03-06 10:36:30 +1300

    Create and store user data, auth state is reflected in UI

---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,22 @@
+export class User {
+  constructor(
+    public email: string,
+    public id: string,
+    private _token: string,
+    private _tokenExpirationDate: Date
+  ) {}
+
+  get token() {
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      return null;
+    }
+    return this._token;
+  }
+
+  get tokenDuration() {
+    if (!this._token) {
+      return 0;
+    }
+    return this._tokenExpirationDate.getTime() - new Date().getTime();
+  }
+}
