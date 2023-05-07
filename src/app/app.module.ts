49ee8bd5905f23bf6b744186f48c7a58b72dea68
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-05-08 11:34:27 +1200

    Adjust styling and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../app/header/header.component';
+import { FooterComponent } from '../app/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
   bootstrap: [AppComponent],
 })
